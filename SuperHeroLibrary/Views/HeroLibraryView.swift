b1eb3c575d5860f624edf4bb544e959572e59f9a
Prof. H <profh@cmu.edu>
2023-10-04 17:45:52 -0400

    Minor tweak to library view

--- SuperHeroLibrary/Views/HeroLibraryView.swift
+++ SuperHeroLibrary/Views/HeroLibraryView.swift
@@ -5,7 +5,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HeroLibraryView: View {
   
   @Environment(\.managedObjectContext) private var moc
 
@@ -93,6 +93,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HeroLibraryView()
     }
 }
